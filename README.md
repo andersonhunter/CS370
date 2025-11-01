aec57495d0e468cb7100c4ee9ce7c0eec81fe87c
Hunter Anderson <129843902+andersonhunter@users.noreply.github.com>
2025-11-01 21:23:18 +0000

    working maybe

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 To run:
-    1. Make sure the bitarray package is installed (`pip install bitarray` if not)
+    1. Make sure the bitarray and sortedcontainers packages are installed (`pip install bitarray`, `pip install sortedcontainers` if not)
     2. Make sure that the rockyou.ISO-8859-1.txt file is in the same dir as the bloom_filter.py file
     3. Make sure that the dictionary.txt file is also in the same dir as the bloom_filter.py file
     4. Run! (Probably with `python3 bloom_filter.py`)
--- bloom_filter.py
+++ bloom_filter.py
@@ -1,5 +1,7 @@
 from bitarray import bitarray
 import hashlib
+import time
+from sortedcontainers import SortedSet
 
 # Define k - the number of hash functions
 K = 13
@@ -14,7 +16,7 @@ class BloomFilter:
     def __init__(self):
         self.bit_array = bitarray(M)
         self.bit_array.setall(0)
-
+        self.db = SortedSet()
 
     def insert_into_bit_array(self, plain: str) -> None:
         # Use double hashing (loop from i = 0 - k-1, calculate h1 and h2, 
@@ -34,13 +36,51 @@ class BloomFilter:
         for i in range(K):
             index = (h1 + i * h2) % M
             if not self.bit_array[index]:
-                print("no")
                 return False
-        print("maybe")
         return True
 
+    def check_membership(self, plain: str) -> bool:
+        return plain in self.db
 
 if __name__ == '__main__':
     # Set up the filter
     bf = BloomFilter()
-    
+    # Read in the rockyou file
+    now_time = time.time()
+    with open("rockyou.ISO-8859-1.txt") as f:
+        # Filter it
+        count = 0
+        for x in f:
+            x = x.replace("\n", "")
+            bf.insert_into_bit_array(x)
+            bf.db.add(x)
+            count += 1
+    print(
+        f'Processed {count} elements from rockyou in {round(time.time() - now_time, 4)} seconds'
+        )
+    now_time = time.time()
+    with open("dictionary.txt", encoding="ISO-8859-1") as f:
+        # Check membership of each item
+        count = 0
+        true_pos, false_pos, false_neg, true_neg = 0, 0, 0, 0
+        for x in f:
+            count += 1
+            x = x.replace("\n", "")
+            # Case 1: In db, pass bloom filter (true positive)
+            # Case 3: Not in db, pass bloom filter (false positive)
+            if bf.check_bit_array(x):
+                if bf.check_membership(x):
+                    true_pos += 1
+                else:
+                    false_pos += 1
+            # Case 2: In db, don't pass bloom filter (false negative)
+            # Case 4: Not in db, don't pass bloom filter (true negative)
+            else:
+                if bf.check_membership(x):
+                    false_neg += 1
+                else:
+                    true_neg += 1
+    print(
+        f'Processed {count} elements from dictionary in {round(time.time() - now_time, 4)} seconds'
+    )
+    print(f'TP, FP, FN, TN = {true_pos, false_pos, false_neg, true_neg}')
